63a2bbf1cd866bb6beca2b6c2f925ec5456989a8
bilalshahzad139 <bilal.shahzad@ibexglobal.com>
2017-03-30 16:55:49 +0500

    Data Scraping - AgilityPack C#

---
+++ Data Scraping - AgilityPack C#/DataScrapingSample/DataScrapingSample/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//This is required to call 'Extension' methods like 'QuerySelectorAll'
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+using System.Net;
+
+
+namespace DataScrapingSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var URL = "http://www.rozee.pk/job/jsearch/q/software+engineer";
+            var _wReq = (HttpWebRequest)WebRequest.Create(URL);
+            _wReq.Method = "GET";
+            String reponseHtml = "";
+
+            HttpWebResponse _wResp = (HttpWebResponse)_wReq.GetResponse();
+            using (System.IO.StreamReader _sr = new System.IO.StreamReader(_wResp.GetResponseStream()))
+            {
+                reponseHtml = _sr.ReadToEnd();
+            }
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(reponseHtml);
+
+            var divs = doc.DocumentNode.QuerySelectorAll(".jlist div.job").ToList();
+
+            foreach (var jobdiv in divs)
+            {
+                var doc1 = new HtmlAgilityPack.HtmlDocument();
+                doc1.LoadHtml(jobdiv.InnerHtml);
+                String jtitle = doc1.DocumentNode.QuerySelector("div.jobt h3 bdi").InnerText;
+                
+            }
+
+        }
+    }
+}
---
+++ Data Scraping - AgilityPack C#/DataScrapingSample/DataScrapingSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataScrapingSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DataScrapingSample")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a19f1d79-5a65-4d81-840f-6a4875a6b3a9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
