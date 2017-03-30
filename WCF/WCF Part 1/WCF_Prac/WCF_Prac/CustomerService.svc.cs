3c705578c6214172ea32b62d2d91e8d849d15f7b
bilalshahzad139 <bilal.shahzad@ibexglobal.com>
2017-03-30 17:20:18 +0500

    WCF (Windows Communication Foundation)

---
+++ WCF/WCF Part 1/WCF_Prac/WCF_Prac/CustomerService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Prac
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CustomerService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CustomerService.svc or CustomerService.svc.cs at the Solution Explorer and start debugging.
+    public class CustomerService : ICustomerService
+    {
+        public String GetMonths(int ageInYear)
+        {
+            return (ageInYear * 12).ToString();
+        }
+    }
+}
---
+++ WCF/WCF Part 1/WCF_TestApp/WCF_TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WCF_TestApp.CustomerServiceManager;
+
+namespace WCF_TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerServiceClient client = new CustomerServiceClient();
+            var result = client.GetMonths(10);
+
+            Console.WriteLine("Months:{0}", result);
+
+            Console.ReadKey();
+        }
+    }
+}
